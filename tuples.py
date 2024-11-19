cd9d25cf8007e0aafdb3c388551cbc02f3f25b30
sachlar <sacharook@gmail.com>
2024-11-19 14:57:45 +0000

    initial commit and readme

---
+++ tuples.py
@@ -0,0 +1,8 @@
+# working with tuples
+# use () immutable 
+dimensions = (200, 100, 75, 50, 25, 5)
+
+for x in dimensions:
+    print(x)
+
+# use tuples when you need a set of values that exist for the life of the program and don't require change
